d6d5a60023f38d3a9526b422662a71b796226b97
Arya Pujianto <aryapujianto2@gmail.com>
2025-06-10 01:04:35 +0700

    dynamic header and fix api

---
+++ pages/account/change-account.vue
@@ -0,0 +1,10 @@
+<template>
+	<NuxtLayout name="detail" title="Ubah Data Akun">
+		<div>asdf</div>
+	</NuxtLayout>
+</template>
+<script setup lang="ts">
+definePageMeta({
+	layout: false
+});
+</script>
