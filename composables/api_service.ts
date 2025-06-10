fe60338ce46a2e8a983dfb6de0768723fc142f77
Arya Pujianto <aryapujianto2@gmail.com>
2025-06-10 09:58:17 +0700

    remove dafault versioning api

--- composables/api_service.ts
+++ composables/api_service.ts
@@ -27,16 +27,16 @@ class ApiServiceImpl implements ApiService {
 	 * Build endpoint URL with version prefix
 	 */
 	private buildEndpointUrl(endpoint: string): string {
-		 const config = useRuntimeConfig()
 		if (endpoint.includes('/auth')) {
 			return endpoint;
 		}
 
-		const apiVersion = config.public.apiVersion;
+		// const config = useRuntimeConfig()
+		// const apiVersion = config.public.apiVersion;
 
-		if (apiVersion) {
-			return `/${apiVersion}${endpoint}`;
-		}
+		// if (apiVersion) {
+		// 	return `/${apiVersion}${endpoint}`;
+		// }
 
 		return endpoint;
 	}
